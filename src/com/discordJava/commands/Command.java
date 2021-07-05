ce0b0dd2f27b0abbcdbf227a6c5c72bf40ea9242
meisZWFLZ <atcurtis08@gmail.com>
2021-07-05 05:34:33 -0700

    fixed em'

--- src/com/discordJava/commands/Command.java
+++ src/com/discordJava/commands/Command.java
@@ -1,21 +1,72 @@
 package com.discordJava.commands;
 
+//
+//public interface Command {
+//
+//    void execute(Input input);
+//
+//    String[] getAliases();
+//    String getName();
+//    String getDescription();
+//    String getUsage();
+//    String getExamples();
+//    Boolean getDmSupport();
+//    Integer[] getArgs();
+//
+//    class Input {
+//        public String prefix;
+//        public Client client;
+//        public String[] args;
+//        public String content;
+//        public String command;
+//        public Message msg;
+//        public Byte version;
+//
+//        public Input(String prefix, Client client, String[] args, String content, String command, Message msg, Byte version) {
+//            this.prefix = prefix;
+//            this.args = args;
+//            this.client = client;
+//            this.content = content;
+//            this.command = command;
+//            this.msg = msg;
+//            this.version = version;
+//        }
+//    }
+//}
+
+
 import com.discordJava.classes.Client;
 import com.discordJava.classes.Message;
 
-public interface Command {
+public abstract class Command {
 
-    void execute(Input input);
+    public abstract void execute(Input input);
 
-    String[] getAliases();
-    String getName();
-    String getDescription();
-    String getUsage();
-    String getExamples();
-    Boolean getDmSupport();
-    Integer[] getArgs();
+    //list of all commands to be used by bot
+    public static final Command[] LIST = new Command[]{
+            new Ping(),
+            new Help()
+    };
 
-    class Input {
+    public final String[] aliases;
+    public final String name;
+    public final String description;
+    public final String usage;
+    public final String examples;
+    public final Boolean dmSupport;
+    public final Integer[] args;
+
+    public Command(String[] aliases, String name, String description, String usage, String examples, Boolean dmSupport, Integer[] args) {
+        this.aliases = aliases;
+        this.name = name;
+        this.description = description;
+        this.usage = usage;
+        this.examples = examples;
+        this.dmSupport = dmSupport;
+        this.args = args;
+    }
+
+    public static class Input {
         public String prefix;
         public Client client;
         public String[] args;
